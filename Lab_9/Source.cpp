a1b34898359045eeab5092603a37a23615490edb
AndrewButeskul <48094797+AndrewButeskul@users.noreply.github.com>
2019-06-08 18:09:22 +0300

    v.1

--- Lab_9/Source.cpp
+++ Lab_9/Source.cpp
@@ -9,54 +9,196 @@ using namespace std;
 struct Mystruct
 {
 	string login;
-	char data_online;
-	clock_t time_online;
-	clock_t time_end;
+	int data_online[3];
+	int begin_time_online[3];
+	int time_end[3];
 	int accept_information;
 	int sent_information;
 };
 
-/*void Input(vector<Mystruct>& user, int n)
+void Input(vector<Mystruct>& user, int n)
 {
+	system("cls");	
 	for (size_t i = 0; i < n; i++)
 	{
-		cout << "Enter your login:  ";
+		cout << "Ваш логин:  ";
 		cin >> user[i].login;
 		cout << endl;
-		cout << "Enter data online:  ";
-		cin >> user[i].data_online;
-		cout << endl;
-		cout << "Enter time  start online:  ";
-		cin >> user[i].time_online;
-		cout << endl;
-		cout << "Enter time end online:  ";
-		cin >> user[i].time_end;
-		cout << endl;
-		
 
+		cout << "Введите дату выхода в онлайн (через пробел д/м/год) - ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			cin >> user[i].data_online[j];
+		}
+		cout << endl;
+		cout << "Введите время начало сеанса: (через пробел ч/м/с) -  ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			cin >> user[i].begin_time_online[j];
+		}
+		cout << endl;
+		cout << "Введите время окончания сеанса: (через пробел ч/м/с) - ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			cin >> user[i].time_end[j];
+		}
+		cout << endl;
 	}
-}*/
+}
+
+void Print(vector<Mystruct>& user, int n)
+{
+	system("cls");
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << "Ваш Логин:" << user[i].login << endl;
+
+		cout << "Дата выхода в онлайн: ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			 cout << user[i].data_online[j] << "/ ";
+		}
+		cout << endl;
+		cout << "Dремя начало сеанса: ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			cout << user[i].begin_time_online[j] << ":";
+		}
+		cout << endl;
+		cout << "Время окончания сеанса: ";
+		for (size_t j = 0; j < 3; j++)
+		{
+			cout << user[i].time_end[j] << ":";
+		}
+		cout << endl << endl;
+	}
+	system("pause");
+}
+
+void Out(vector<Mystruct>& user, int n, ofstream &out)
+{
+	out.open("Data.txt");
+	if (!out.is_open())
+		cout << "Error open!\n";
+	else
+	{
+		for (size_t i = 0; i < n; i++)
+		{
+			out << user[i].login << endl;
+
+			for (size_t j = 0; j < 3; j++)
+			{
+				out << user[i].data_online[j] << " ";
+			}
+			out << endl;
+			for (size_t j = 0; j < 3; j++)
+			{
+				out << user[i].begin_time_online[j] << " ";
+			}
+			out << endl;
+			for (size_t j = 0; j < 3; j++)
+			{
+				out << user[i].time_end[j] << " ";
+			}
+			out << endl << endl;
+		}
+	}
+	cout << "Данные успешно сохранены в файл" << endl;
+	system("pause");
+}
+
+void Fin(vector<Mystruct>& user, int n, ifstream &fin)
+{	
+	fin.open("Data.txt");
+	if (!fin.is_open())
+		cout << "Error open!\n";
+	else
+	{
+		for (size_t i = 0; i < n; i++)
+		{
+			fin >> user[i].login;
+			for (size_t j = 0; j < 3; j++)
+			{
+				fin >> user[i].data_online[j];
+			}
+			for (size_t j = 0; j < 3; j++)
+			{
+				fin >> user[i].begin_time_online[j];
+			}
+			for (size_t j = 0; j < 3; j++)
+			{
+				fin >> user[i].time_end[j];
+			}
+		}
+		cout << "Данные успешно импортированы\n";
+	}
+	system("pause");
+}
+
+void Menu(vector<Mystruct>& user, int n, ofstream &out, ifstream &fin)
+{
+    link:
+	system("cls");
+	int selection = 0;
+	cout << "Выберете раздел:\n" <<
+		"1. Посмотреть данные о пользователях.\n" <<
+		"2. Записать пользователей в файл.\n" <<
+		"3. Импортировать данные.\n" <<
+		"4. Количество сеансов выхода в онлайн в прошлом году.\n" <<
+		"5. Максимальная длительность сеанса.\n" <<
+		"6. Выход.\n";
+	cin >> selection;
+	switch (selection)
+	{
+	case 1:
+		Print(user, n);
+		goto link;
+		break;
+	case 2:
+		Out(user, n, out);
+		goto link;
+		break;
+	case 3:
+		Fin(user, n, fin);
+		goto link;
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
 
 int main()
 {
-	/*int n;
-	cout << "Enter amount users:" << endl;
+	setlocale(LC_ALL, "ru");
+	ofstream out;
+	ifstream fin;
+	int n, selection = 0;
+	cout << "Введите количество пользователей:" << endl;
 	cin >> n;
-	vector<Mystruct> user(n);*/
-	Mystruct user;
+	vector<Mystruct> user(n);
 
-	cout << "Enter your login:  ";
-	cin >> user.login;
-	cout << endl;
-	cout << "Enter data online:  ";
-	cin >> user.data_online;
-	cout << endl;
-	cout << "Enter time  start online:  ";
-	cin >> user.time_online;
-	cout << endl;
-	cout << "Enter time end online:  ";
-	cin >> user.time_end;
-	cout << endl;
+	cout << "Вы хотите добавить заполнить или импортировать готовые данные? ( 1 - Заполнить | 2 - Импортировать)\n";
+	cin >> selection;
+
+	switch (selection)
+	{
+	case 1: 		
+		Input(user, n);
+		break;
+	case 2:
+		Menu(user, n, out, fin);
+		break;
+	default:
+		exit(0);
+		break;
+	}
 
 	system("pause");
 	return 0;
